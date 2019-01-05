9771bf7fccecdd238dad7d8378146f5729d98965
take@elgraiv <4583273+elgraiv-take@users.noreply.github.com>
2019-01-06 02:32:19 +0900

    ScrollBarのスタイルを追加

---
+++ src/Aphroreader/Aphroreader/Main/ContentThumbnailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elgraiv.Aphroreader.Main
+{
+    [Util.DataTemplateTarget(typeof(ContentThumbnailView))]
+    class ContentThumbnailViewModel
+    {
+    }
+}
--- src/Aphroreader/Aphroreader/Main/MainWindowViewModel.cs
+++ src/Aphroreader/Aphroreader/Main/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +8,20 @@ using System.Threading.Tasks;
 namespace Elgraiv.Aphroreader.Main
 {
     [Util.DataTemplateTarget(typeof(MainWindowView))]
-    class MainWindowViewModel
+    class MainWindowViewModel:ViewModelBase
     {
+        private ObservableCollection<ContentThumbnailViewModel> _contents;
+        public ReadOnlyObservableCollection<ContentThumbnailViewModel> Contents { get; } 
+
+        public MainWindowViewModel()
+        {
+            _contents = new ObservableCollection<ContentThumbnailViewModel>();
+            Contents = new ReadOnlyObservableCollection<ContentThumbnailViewModel>(_contents);
+
+            for(var i = 0; i < 10; i++)
+            {
+                _contents.Add(new ContentThumbnailViewModel());
+            }
+        }
     }
 }
