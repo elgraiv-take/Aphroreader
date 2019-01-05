0efb04212806f90e7f20c59bc47be6cdd5d8a8a1
take@elgraiv <4583273+elgraiv-take@users.noreply.github.com>
2019-01-05 15:36:57 +0900

    とりあえずベースのウィンドウを作成

---
+++ src/Aphroreader/Aphroreader/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Elgraiv.Aphroreader
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            InitializeDataTemplate();
+
+            var mainWindow = new BaseWindow();
+            mainWindow.DataContext = new BaseWindowViewModel()
+            {
+                Content=new Main.MainWindowViewModel(),
+            };
+            MainWindow = mainWindow;
+            mainWindow.Show();
+        }
+
+
+        private void InitializeDataTemplate()
+        {
+            AddDataTemplate(typeof(Main.MainWindowViewModel), typeof(Main.MainWindowView));
+        }
+
+        private void AddDataTemplate(Type viewModelType,Type viewType)
+        {
+            Resources.Add(
+                new DataTemplateKey(viewModelType),
+                new DataTemplate(viewModelType) {
+                    VisualTree = new FrameworkElementFactory(viewType)
+                }
+                );
+        }
+    }
+}
---
+++ src/Aphroreader/Aphroreader/Util/ButtonUtil.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Elgraiv.Aphroreader.Util
+{
+    public enum ButtonCornerStyle
+    {
+        Default,
+        System,
+    }
+    public static class ButtonUtil
+    {
+        public static readonly DependencyProperty CornerStyleProperty =
+        DependencyProperty.RegisterAttached(
+            "CornerStyle",
+            typeof(ButtonCornerStyle),
+            typeof(ButtonUtil),
+            new PropertyMetadata(ButtonCornerStyle.Default));
+
+        public static ButtonCornerStyle GetCornerStyle(Button obj)
+        {
+            return (ButtonCornerStyle)obj.GetValue(CornerStyleProperty);
+        }
+
+        public static void SetCornerStyle(Button obj, ButtonCornerStyle value)
+        {
+            obj.SetValue(CornerStyleProperty, value);
+        }
+    }
+}
